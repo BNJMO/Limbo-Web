f5b505b59d2f9621c2c96174c98195a1ac136490
BNJMO <bnjmo.med@gmail.com>
2025-11-24 20:43:03 +0100

    Remove options controls and simplify profit label

--- src/controlPanel/controlPanel.js
+++ src/controlPanel/controlPanel.js
@@ -99,10 +99,6 @@ export class ControlPanel extends EventTarget {
     this.buildToggle();
     this.buildBetAmountDisplay();
     this.buildBetControls();
-    this.buildMinesLabel();
-    this.buildMinesSelect();
-    this.buildGemsLabel();
-    this.buildGemsDisplay();
     this.buildModeSections();
     this.buildFooter();
 
@@ -748,8 +744,7 @@ export class ControlPanel extends EventTarget {
 
   updateTotalProfitLabel() {
     if (!this.profitOnWinLabel) return;
-    const formattedMultiplier = this.totalProfitMultiplier.toFixed(2);
-    this.profitOnWinLabel.textContent = `Total Profit (${formattedMultiplier}x)`;
+    this.profitOnWinLabel.textContent = "Total Profit";
   }
 
   setTotalProfitMultiplier(value) {
